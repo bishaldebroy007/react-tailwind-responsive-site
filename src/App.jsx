6a492688e2dbff29f259f4e9e19d74225c4fd8da
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-18 15:05:31 +0600

    working on the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import react from 'react';
+import NavBar from './components/NavBar';
 
 function App() {
 
 
   return (
     <>
-
+      <NavBar />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+
+
+const NavBar = () => {
+    return (
+        <div className=" text-white p-4">
+            <h1 className="text-xl w-full text-bold text-green-500 font-extrabold">Dummy Site.</h1>
+            <ul className="flex justify-between w-full p-3">
+                <li>Home</li>
+                <li>Company</li>
+                <li>Resources</li>
+                <li>Contact</li>
+                <li>About Me</li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar
