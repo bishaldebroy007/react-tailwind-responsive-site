02c6a99b654e594e53d3af3ed90053fe00c0d1f1
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-18 15:59:18 +0600

    Menu in work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
-
+import { MdOutlineMenuOpen } from "react-icons/md";
 
 const NavBar = () => {
     return (
-        <div className=" text-white p-4">
-            <h1 className="text-xl w-full text-bold text-green-500 font-extrabold">Dummy Site.</h1>
-            <ul className="flex justify-between w-full p-3">
-                <li>Home</li>
-                <li>Company</li>
-                <li>Resources</li>
-                <li>Contact</li>
-                <li>About Me</li>
+        <div className=" text-white flex items-center justify-between h-24 mx-auto px-4">
+            <h1 className="text-xl w-full text-green-500 font-black">Dummy Site.</h1>
+            <ul className="flex hidden uppercase font-semibold">
+                <li className="p-3">Home</li>
+                <li className="p-3">Company</li>
+                <li className="p-3">Resources</li>
+                <li className="p-3">Contact</li>
+                <li className="p-3">About</li>
             </ul>
+            <div>
+                <MdOutlineMenuOpen size={30} />
+            </div>
         </div>
     )
 }
