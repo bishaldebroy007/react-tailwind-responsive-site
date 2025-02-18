ded48d29e7750a798dad540e57bf3310545b16c9
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-18 17:57:10 +0600

    search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import react from 'react';
 import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <SearchBar />
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,23 +4,23 @@ const NavBar = () => {
     return (
         <div className=" text-white flex items-center justify-between h-24 mx-auto px-4">
             <h1 className="text-xl w-full text-green-500 font-black cursor-pointer">Dummy Site.</h1>
-            <ul className="flex hidden uppercase font-semibold cursor-pointer">
-                <li className="p-3">Home</li>
-                <li className="p-3">Company</li>
-                <li className="p-3">Resources</li>
-                <li className="p-3">Contact</li>
-                <li className="p-3">About</li>
+            <ul className="flex hidden uppercase font-semibold">
+                <li className="p-3 cursor-pointer">Home</li>
+                <li className="p-3 cursor-pointer">Company</li>
+                <li className="p-3 cursor-pointer">Resources</li>
+                <li className="p-3 cursor-pointer">Contact</li>
+                <li className="p-3 cursor-pointer">About</li>
             </ul>
             <div>
                 <MdOutlineMenuOpen size={30} className="cursor-pointer" />
             </div>
-            <div className="fixed left-0 top-0 w-[60%] uppercase font-semibold cursor-pointer">
-                <ul className="pt-23 uppercase font-semibold cursor-pointer">
-                    <li className="p-3">Home</li>
-                    <li className="p-3">Company</li>
-                    <li className="p-3">Resources</li>
-                    <li className="p-3">Contact</li>
-                    <li className="p-3">About</li>
+            <div className="fixed left-0 top-0 h-full  w-[40%] uppercase font-semibold">
+                <ul className="pt-23 uppercase font-semibold">
+                    <li className="p-3 cursor-pointer">Home</li>
+                    <li className="p-3 cursor-pointer">Company</li>
+                    <li className="p-3 cursor-pointer">Resources</li>
+                    <li className="p-3 cursor-pointer">Contact</li>
+                    <li className="p-3 cursor-pointer">About</li>
                 </ul>
             </div>
         </div>
