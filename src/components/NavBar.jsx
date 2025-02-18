77382ce02b62866be1ed7adc198f3ff37fca581e
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-18 16:25:44 +0600

    navigation bar work update to the left

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import { MdOutlineMenuOpen } from "react-icons/md";
 const NavBar = () => {
     return (
         <div className=" text-white flex items-center justify-between h-24 mx-auto px-4">
-            <h1 className="text-xl w-full text-green-500 font-black">Dummy Site.</h1>
-            <ul className="flex hidden uppercase font-semibold">
+            <h1 className="text-xl w-full text-green-500 font-black cursor-pointer">Dummy Site.</h1>
+            <ul className="flex hidden uppercase font-semibold cursor-pointer">
                 <li className="p-3">Home</li>
                 <li className="p-3">Company</li>
                 <li className="p-3">Resources</li>
@@ -12,7 +12,16 @@ const NavBar = () => {
                 <li className="p-3">About</li>
             </ul>
             <div>
-                <MdOutlineMenuOpen size={30} />
+                <MdOutlineMenuOpen size={30} className="cursor-pointer" />
+            </div>
+            <div className="fixed left-0 top-0 w-[60%] uppercase font-semibold cursor-pointer">
+                <ul className="pt-23 uppercase font-semibold cursor-pointer">
+                    <li className="p-3">Home</li>
+                    <li className="p-3">Company</li>
+                    <li className="p-3">Resources</li>
+                    <li className="p-3">Contact</li>
+                    <li className="p-3">About</li>
+                </ul>
             </div>
         </div>
     )
