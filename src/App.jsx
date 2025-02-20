67e7f703d066b7b604fb81894bb523864a724e95
bishaldebroy007 <bishaldebroy2000@gmail.com>
2025-02-20 13:26:40 +0600

    Converted to flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import react from 'react';
 import NavBar from './components/NavBar';
-import SearchBar from './components/SearchBar';
+
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <SearchBar />
+
     </>
   )
 }
